864e49e5264bfee093877bc0b4458197499af6b9
Philipp Muench <philipp.muench@helmholtz-hzi.de>
2024-06-04 22:39:52 +0200

    working deepg and keras

--- build.sh
+++ build.sh
@@ -7,6 +7,22 @@ echo "Installing the scripts..."
 cp $SRC_DIR/virusnet.py $PREFIX/bin/virusnet  # copy it to the bin directory
 cp $SRC_DIR/predict.r $PREFIX/bin/predict.r  # copy it to the bin directory
 
-# Install the R package
-echo "Installing the R package from GitHub..."
-R -e "devtools::install_github('genomenet/deepg')"
+echo "R is located at: $(which R)"
+
+
+# Output R library paths using .libPaths()
+echo "R library paths:"
+$(which R) -e ".libPaths()"
+
+# Output the R_LIBS and R_LIBS_USER environment variables
+echo "R_LIBS: $R_LIBS"
+echo "R_LIBS_USER: $R_LIBS_USER"
+
+# Set the R library directory
+mkdir -p "$PREFIX/lib/R/library"
+chmod -R u+w "$PREFIX/lib/R/library"
+R_LIBS_SITE="$PREFIX/lib/R/library"
+
+"$BUILD_PREFIX/bin/R" -e "remotes::install_github('GenomeNet/deepg', lib='$PREFIX/lib/R/library')" # .libPaths() should be $BUILD_PREFIX/lib/R/library 
+
+
