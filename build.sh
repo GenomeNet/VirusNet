6bd7d02b4c2851d42c9fba62f6aa93547581c026
Philipp Muench <philipp.muench@helmholtz-hzi.de>
2024-06-04 14:10:29 +0200

    add DeepG R

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ mkdir -p $PREFIX/bin
 echo "Installing the scripts..."
 cp $SRC_DIR/virusnet.py $PREFIX/bin/virusnet  # copy it to the bin directory
 cp $SRC_DIR/predict.r $PREFIX/bin/predict.r  # copy it to the bin directory
+
+# Install the R package
+echo "Installing the R package from GitHub..."
+R -e "devtools::install_github('genomenet/deepg')"
