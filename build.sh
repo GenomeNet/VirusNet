6163f8b5accf701a5a5a345eb2534c7b2875c8aa
Philipp Muench <philipp.muench@helmholtz-hzi.de>
2024-06-05 10:44:36 +0200

    update links

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ mkdir -p $PREFIX/bin
 
 # Install the Python script
 echo "Installing the scripts..."
-cp $SRC_DIR/virusnet.py $PREFIX/bin/virusnet  # copy it to the bin directory
-cp $SRC_DIR/predict.r $PREFIX/bin/predict.r  # copy it to the bin directory
+cp $SRC_DIR/virusnet.py $PREFIX/bin/virusnet
+cp $SRC_DIR/predict.r $PREFIX/bin/predict.r
 
 echo "R is located at: $(which R)"
 
@@ -22,7 +22,6 @@ echo "R_LIBS_USER: $R_LIBS_USER"
 mkdir -p "$PREFIX/lib/R/library"
 chmod -R u+w "$PREFIX/lib/R/library"
 R_LIBS_SITE="$PREFIX/lib/R/library"
-
 "$BUILD_PREFIX/bin/R" -e "remotes::install_github('GenomeNet/deepg', lib='$PREFIX/lib/R/library')" # .libPaths() should be $BUILD_PREFIX/lib/R/library 
 
 
