a9f104f8990b6ec0a7b3df3341fe89fae0561f43
Philipp Muench <philipp.muench@helmholtz-hzi.de>
2024-06-07 13:34:40 +0200

    working bin/genus model

--- build.sh
+++ build.sh
@@ -6,22 +6,11 @@ mkdir -p $PREFIX/bin
 echo "Installing the scripts..."
 cp $SRC_DIR/virusnet.py $PREFIX/bin/virusnet
 cp $SRC_DIR/predict.r $PREFIX/bin/predict.r
+cp $SRC_DIR/utils.r $PREFIX/bin/utils.r
 
-echo "R is located at: $(which R)"
-
-
-# Output R library paths using .libPaths()
-echo "R library paths:"
-$(which R) -e ".libPaths()"
-
-# Output the R_LIBS and R_LIBS_USER environment variables
-echo "R_LIBS: $R_LIBS"
-echo "R_LIBS_USER: $R_LIBS_USER"
 
 # Set the R library directory
 mkdir -p "$PREFIX/lib/R/library"
 chmod -R u+w "$PREFIX/lib/R/library"
 R_LIBS_SITE="$PREFIX/lib/R/library"
-"$BUILD_PREFIX/bin/R" -e "remotes::install_github('GenomeNet/deepg', lib='$PREFIX/lib/R/library')" # .libPaths() should be $BUILD_PREFIX/lib/R/library 
-
-
+"$BUILD_PREFIX/bin/R" -e "remotes::install_github('GenomeNet/deepg', lib='$PREFIX/lib/R/library')" 
--- virusnet/predict.r
+++ virusnet/predict.r
@@ -2,20 +2,26 @@
 
 # Suppress TF messages
 Sys.setenv(TF_CPP_MIN_LOG_LEVEL = 3)
+
 # Set library paths to the Conda environment's R library path
-.libPaths(c(paste0(Sys.getenv("PREFIX"), "/lib/R/library")))
+#.libPaths(c(paste0(Sys.getenv("PREFIX"), "/lib/R/library")))
 
 # Load libraries
 suppressWarnings(suppressPackageStartupMessages({
   library(deepG)
-}))
-
-suppressPackageStartupMessages({
   library(magrittr)
+  library(microseq)
   library(optparse)
   library(ggplot2)
   library(zoo)
-})
+  library(keras)
+  library(reticulate)
+}))
+
+source("virusnet/utils.r")
+
+#use_python(paste0(Sys.getenv("PREFIX"), "/bin/python"), required = TRUE)
+# use_python("/home/pmuench/micromamba/envs/build-env2/conda-bld/virusnet-0_1717753851740/_test_e/bin/python")
 
 args <- commandArgs(trailingOnly = TRUE)
 
@@ -25,12 +31,59 @@ for (i in seq(1, length(args), 2)) {
   arg_list[[gsub("--", "", args[i])]] <- args[i + 1]
 }
 
-cat("\nGenus Model Summary:\n")
+print(arg_list)
+
+message("Checking input file")
+
+# Read the FASTA file using microseq
+fasta_data <- readFasta(arg_list$input)
+
+# Count the number of FASTA entries
+num_fasta_entries <- nrow(fasta_data)
+
+# Print the number of FASTA entries
+message(paste("Number of FASTA entries in the file:", num_fasta_entries))
+
+message("Loading genus model...")
 model_genus <- keras::load_model_hdf5(arg_list$model_genus, compile = FALSE)
-summary(model_genus)
 
-# Load and summarize each model
-cat("Binary Model Summary:\n")
-model_binary <- deepG::load_cp(arg_list$model_binary)
-summary(model_binary)
+message("Loading binary model...")
+model_binary <- keras::load_model_hdf5(arg_list$model_binary, custom_objects = custom_objects)
 
+
+# predict using genus model
+pred <- predict_model(
+    output_format = "by_entry_one_file",
+    model = model_genus,
+    layer_name = "dense_3",
+    path_input = arg_list$input,
+    round_digits = 3,
+    step = 1000,
+    batch_size = 5120,
+    verbose = FALSE,
+    return_states = FALSE,
+    padding = "standard",
+    mode = "label",
+    format = "fasta",
+    filename = "output_genus.h5",
+    return_int = FALSE)
+
+head(pred)
+
+length(model_binary$input_shape)
+
+pred <- predict_model(
+    output_format = "by_entry_one_file",
+    model = model_binary,
+    layer_name = "dense_26",
+    path_input = arg_list$input,
+    round_digits = 3,
+    step = 1000,
+    batch_size = 512,
+    verbose = FALSE,
+    return_states = FALSE,
+    padding = "standard",
+    mode = "label",
+    format = "fasta",
+    filename = "output_bin.h5",
+    return_int = length(model_binary$input_shape) == 2)
