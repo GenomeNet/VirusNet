b826955b4b50a8c76e807998d024f91f62882369
Philipp Muench <philipp.muench@helmholtz-hzi.de>
2024-06-09 14:28:19 +0200

    add binary and genus model

--- virusnet/virusnet.py
+++ virusnet/virusnet.py
@@ -59,18 +59,29 @@ def check_files(download_path):
             return False
     return True
 
-def run_prediction(input, output, model_paths):
+def run_prediction(input, output, model_paths, step_size=1000, batch_size=100, mode='binary', metagenome=False):
     """
     Function to run the R script for virus prediction using the specified arguments.
     """
-    r_script_path = os.path.join(os.path.dirname(__file__), "predict.r")
+    if mode == 'binary':
+        if metagenome:
+            r_script_path = os.path.join(os.path.dirname(__file__), "predict_binary_metagenome.r")
+        else:
+            r_script_path = os.path.join(os.path.dirname(__file__), "predict_binary.r")
+    elif mode == 'genus':
+        r_script_path = os.path.join(os.path.dirname(__file__), "predict_genus.r")
+    else:
+        raise ValueError(f"Invalid mode: {mode}")
+
     command = [
-        "Rscript", r_script_path, 
-        '--input', input, 
-        '--output', output, 
+        "Rscript", r_script_path,
+        '--input', input,
+        '--output', output,
         '--model_binary', model_paths['binary_model'], 
         '--model_genus', model_paths['genus_model'], 
-        '--labels_genus', model_paths['genus_labels']
+        '--labels_genus', model_paths['genus_labels'],
+        '--step_size', str(step_size),
+        '--batch_size', str(batch_size)
     ]
     subprocess.run(command)
 
@@ -86,7 +97,11 @@ if __name__ == "__main__":
     predict_parser.add_argument('--input', type=str, required=True)
     predict_parser.add_argument('--output', type=str, required=True)
     predict_parser.add_argument('--path', type=str, default=os.path.expanduser('~/.virusnet'))
-
+    predict_parser.add_argument('--step_size', type=int, default=1000, help='Step size for prediction')
+    predict_parser.add_argument('--batch_size', type=int, default=100, help='Batch size for prediction')
+    predict_parser.add_argument('--mode', type=str, choices=['binary', 'genus'], default='binary', help='Prediction mode')
+    predict_parser.add_argument('--metagenome', action='store_true', help='Enable metagenome mode (only applicable for binary mode)')
+    
     args = parser.parse_args()
 
     if args.command == 'download':
@@ -94,6 +109,6 @@ if __name__ == "__main__":
     elif args.command == 'predict':
         if check_files(args.path):
             model_paths = {key: os.path.join(args.path, os.path.basename(info['url'])) for key, info in json.load(open('models.json')).items()}
-            run_prediction(args.input, args.output, model_paths)
+            run_prediction(args.input, args.output, model_paths, args.step_size, args.batch_size, args.mode, args.metagenome)
         else:
             print("Model files are missing or corrupted. Please download them again.")
